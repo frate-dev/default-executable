7acd319a9ff30258baf025bce68c666497fd6052
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-08 09:20:04 -0500

    adding lua scripts

--- scripts/toolchains.lua
+++ scripts/toolchains.lua
@@ -1,5 +1,6 @@
 local toolchains_str = ""
 
+print("project.name " .. project.project_name)
 
 for _, toolchain in pairs(project.toolchains) do
   toolchains_str = toolchains_str .. "include(toolchains/" .. toolchain .. ".cmake)\n"
