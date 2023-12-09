a6376f094a2c565694fd774bc3949b40d3a4ef2f
Jon-Michael Hartway <jhartway99@gmail.com>
2023-12-08 21:10:02 -0500

    updated

--- scripts/ccache.lua
+++ scripts/ccache.lua
@@ -1,18 +1,18 @@
-local cache_str = ""
-
-cache_str = cache_str .. "CPMAddPackage(\n"
-cache_str = cache_str .. "  NAME Ccache.cmake\n"
-cache_str = cache_str .. "  GITHUB_REPOSITORY TheLartians/Ccache.cmake\n"
-cache_str = cache_str .. "  VERSION 1.2\n"
-cache_str = cache_str .. ")\n"
-cache_str = cache_str .. "find_program(CCACHE_FOUND ccache)\n"
-cache_str = cache_str .. "if(CCACHE_FOUND)\n"
-cache_str = cache_str .. "  set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)\n"
-cache_str = cache_str .. "  set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)\n"
-cache_str = cache_str .. " set(CMAKE_C_COMPILER_LAUNCHER ccache)\n"
-cache_str = cache_str .. " set(CMAKE_CXX_COMPILER_LAUNCHER ccache)\n"
-cache_str = cache_str .. "else()\n"
-cache_str = cache_str .. "  message(WARNING \"ccache not found\")\n"
-cache_str = cache_str .. "endif()\n"
+local cache_str = [[
+CPMAddPackage(
+  NAME Ccache.cmake
+  GITHUB_REPOSITORY TheLartians/Ccache.cmake
+  VERSION 1.2
+)
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+  set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+  set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+ set(CMAKE_C_COMPILER_LAUNCHER ccache)
+ set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
+else()
+  message(WARNING "ccache not found")
+endif()
+]]
 
 return cache_str
