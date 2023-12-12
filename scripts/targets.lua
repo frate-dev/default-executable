48979c02f0d737b67ec3eac533a0964217207a14
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-11 19:45:11 -0500

    lib changes

--- scripts/targets.lua
+++ scripts/targets.lua
@@ -3,4 +3,7 @@ local target_str = ""
 for _, target in pairs(project.dependencies) do
   target_str = target_str .. "target_link_libraries(" .. project.name .. " " .. target.target_link .. ")\n"
 end
+for _, lib in pairs(project.libs) do
+  target_str = target_str .. "target_link_libraries(" .. project.name .. " " .. lib .. ")\n"
+end
 return target_str
