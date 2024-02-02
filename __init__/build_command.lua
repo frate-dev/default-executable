3074e512bcdd195af90f3bb5c1069c884460d244
Jon-Michael Hartway <jhartway99@gmail.com>
2024-02-02 15:39:00 -0500

    added scripts and updated build command init

--- __init__/build_command.lua
+++ __init__/build_command.lua
@@ -1,8 +1,9 @@
-project.build_command = frate.format([[
+project.set_build_command(frate.format([[
 cmake -DCMAKE_BUILD_TYPE={%current_mode%}  
-make -j ${nproc} ./{%build_dir%}/{%name%}
+make -j {%jobs%} ./{%build_dir%}/{%name%}
 ]], {
     current_mode = project.current_mode,
     build_dir = project.build_dir,
-    name = project.name
-})
+    name = project.name,
+    jobs = plocal.get_requested_jobs()
+}))
